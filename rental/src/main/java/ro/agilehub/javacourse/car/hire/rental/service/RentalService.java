3be30380650b36e028ee6ffecdd9bbb56816ff28
loredna <loredana_surugiu@yahoo.com>
2021-01-11 20:52:23 +0200

    Homework 9: Extract rental module to a new microservice

--- rental/src/main/java/ro/agilehub/javacourse/car/hire/rental/service/RentalService.java
+++
@@ -1,9 +0,0 @@
-package ro.agilehub.javacourse.car.hire.rental.service;
-
-import ro.agilehub.javacourse.car.hire.rental.domain.RentalDO;
-
-public interface RentalService {
-
-    RentalDO createNewRental(RentalDO rentalDO);
-
-}
