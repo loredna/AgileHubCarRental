958c4301bf0ff77d4f9b0ad48e833dfeb5307278
loredna <loredana_surugiu@yahoo.com>
2021-01-25 19:57:09 +0200

    Add new imtegration tests

--- user.management/src/test/java/ro/agilehub/javacourse/car/hire/user/controller/UserControllerIntegrationTest.java
+++ user.management/src/test/java/ro/agilehub/javacourse/car/hire/user/controller/UserControllerIntegrationTest.java
@@ -12,7 +12,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ro.agilehub.javacourse.car.hire.api.model.UserDTO;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,8 +34,8 @@ public class UserControllerIntegrationTest {
     @Test
     @WithMockUser("jack")
     public void whenAddUserOk_thenFindById() throws Exception {
-        final String email = "loredana_costea@test.com";
-        final String username = "loredanacosteatest";
+        final String email = "test@gmail.com";
+        final String username = "test";
         var input = new UserDTO().email(email).username(username);
 
         var postResult = mvc.perform(post("/user")
@@ -51,9 +54,68 @@ public class UserControllerIntegrationTest {
 
         assertEquals(email, userDTO.getEmail());
 
-        mvc.perform(delete("/user/" + createdDTO.getId())
+        mvc.perform(delete("/user/" + createdDTO.getId()))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+    }
+
+    @Test
+    @WithMockUser("jack")
+    public void whenAddUser_thenGetAllUsers_AndExpectAtLeastOneToBeFound() throws Exception {
+        final String email = "test@gmail.com";
+        final String username = "test";
+        var input = new UserDTO().email(email).username(username);
+
+        var postResult = mvc.perform(post("/user")
                 .contentType("application/json")
                 .content(objectMapper.writeValueAsString(input)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var getResult = mvc.perform(get("/user"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var listOfUsers = objectMapper.readValue(getResult.getResponse().getContentAsString(), List.class);
+
+        System.out.println(listOfUsers);
+        assertTrue(listOfUsers.size() > 0);
+
+        var createdDTO = objectMapper.readValue(postResult.getResponse().getContentAsString(), UserDTO.class);
+
+        mvc.perform(delete("/user/" + createdDTO.getId()))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+    }
+
+    @Test
+    @WithMockUser("jack")
+    public void whenAddUser_thenUpdateTestUserEmail() throws Exception {
+        final String email = "test@gmail.com";
+        final String username = "test";
+        var newUser = new UserDTO().email(email).username(username);
+
+        var postResult = mvc.perform(post("/user")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(newUser)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var createdDTO = objectMapper.readValue(postResult.getResponse().getContentAsString(), UserDTO.class);
+
+        final String newEmail = "test2@gmail.com";
+        var userToUpdate = new UserDTO().id(createdDTO.getId()).email(newEmail);
+
+        var patchResult = mvc.perform(patch("/user")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(userToUpdate)))
+                .andExpect(status().isOk())
+                .andReturn();
+        var updatedDTO = objectMapper.readValue(patchResult.getResponse().getContentAsString(), UserDTO.class);
+
+        assertEquals(newEmail, updatedDTO.getEmail());
+
+        mvc.perform(delete("/user/" + createdDTO.getId()))
                 .andExpect(status().is2xxSuccessful())
                 .andReturn();
     }
